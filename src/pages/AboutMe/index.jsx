332e2ab0bec394294d59c3d8503d214a0605b80f
Thiago <thiago.wander22@gmail.com>
2024-11-14 19:48:15 -0300

    :sparkles: about me page

---
+++ src/pages/AboutMe/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "../../components/Navbar";
+import "./AboutMePage.css";
+
+function AboutMePage() {
+    return (
+        <div className="aboutme-wrapper">
+            <h1>About Me</h1>
+        </div>
+    )
+}
+
+export default AboutMePage;
