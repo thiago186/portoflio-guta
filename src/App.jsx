4dbe21469dbdffe19bf3f7221b56b2d85751a20a
Thiago <thiago.wander22@gmail.com>
2024-11-14 19:47:13 -0300

    :art: changed navbar filename

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar'
 import { Router } from 'react-router-dom'
 
 function App() {
