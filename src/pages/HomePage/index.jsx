99a95573044d63b3e77a57d3cb5d75f253046bba
Thiago <thiago.wander22@gmail.com>
2024-11-18 10:02:10 -0300

    :art: added theme to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,9 +4,12 @@ import Navbar from "../../components/Navbar";
 import "./HomePage.css";
 
 function HomePage() {
+    const theme='light';
+    console.log(theme);
+
     return (
         <div className="homepage-wrapper">
-            <Navbar />
+            <Navbar theme={theme}/>
             <div className="homepage-icon-container">
                 <img src="/guta.svg" alt="Guta Icon" />
             </div>
